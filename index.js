a44a9473895117ed51f9412c0f9e7fe80ddeb0cc
hongmaoxiao <769501294@qq.com>
2018-10-26 00:36:06 +0800

    Update: Include results in the callback

--- index.js
+++ index.js
@@ -4,34 +4,34 @@ var domain = require('domain');
 var EE = require('events').EventEmitter;
 
 function asyncDone(fn, done) {
-  function successCallback() {
-    return done();
+  function onSuccess() {
+    return done(undefined, result);
   }
 
-  function errorCallback(err) {
+  function onError(err) {
     return done(err);
   }
 
   var d = domain.create();
-  d.on('error', errorCallback);
+  d.on('error', onError);
   var domainBoundFn = d.bind(fn);
 
   function asyncRunner() {
-    var result = domainBoundFn(errorCallback);
+    var result = domainBoundFn(onError);
 
     if (result && result instanceof EE) {
       d.add(result);
-      result.once('end', successCallback).once('close', successCallback);
+      result.once('end', onSuccess).once('close', onSuccess);
       return;
     }
 
     if (result && typeof result.then === 'function') {
-      result.then(successCallback, errorCallback);
+      result.then(onSuccess, onError);
       return;
     }
 
     if (fn.length === 0) {
-      done();
+      done(undefined, result);
     }
   }
 
