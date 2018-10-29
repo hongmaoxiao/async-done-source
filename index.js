ae3797d7c1c5ca9fd9dad457f754518985fc493c
didi <fengxiaomao@didichuxing.com>
2018-10-30 00:27:47 +0800

    Fix: Change & tests for failing child processes (fixes #24)

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function asyncDone(fn, cb) {
     if (result && typeof result.on === 'function') {
       // assume node stream
       d.add(result);
-      eos(exhaust(result), eosConfig, onSuccess);
+      eos(exhaust(result), eosConfig, done);
       return;
     }
 
